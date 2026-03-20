65c774127ab8cc5ef9559c06090e98a6c6980634
Kartik Mehra <kartik742m@gmail.com>
2026-03-20 12:32:00 +0530

    chore: cleanup and final organizational fixes

---
+++ client/src/mocks/handlers.js
@@ -0,0 +1,14 @@
+import { http, HttpResponse } from 'msw'
+
+export const handlers = [
+  http.get('/api/products', () => {
+    return HttpResponse.json([
+      { id: 1, name: 'Test Product', price: 99.99, category: 'Test', image: '' }
+    ])
+  }),
+  http.get('/api/categories', () => {
+    return HttpResponse.json([
+      { id: 1, name: 'Test Category', count: 1, image: '' }
+    ])
+  })
+]
