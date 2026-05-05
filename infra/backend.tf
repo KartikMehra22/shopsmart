8422a8ee3d8e7d245a4c1fb59eb2b8523b6d9c1a
Kartik Mehra <kartik742m@gmail.com>
2026-05-05 18:37:34 +0530

    feat(infra): wire backend.tf to bootstrap-created state bucket

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "shopsmart-tfstate-PLACEHOLDER"
+    bucket  = "shopsmart-tfstate-df62308f"
     key     = "shopsmart/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
