ab83513e65cef3463684b3ebde966fec91fdfc5b
Kartik Mehra <kartik742m@gmail.com>
2026-05-05 10:45:50 +0530

    fix(infra): relax Terraform required_version to >= 1.5

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.6"
+  required_version = ">= 1.5"
   required_providers {
     aws    = { source = "hashicorp/aws", version = "~> 5.60" }
     random = { source = "hashicorp/random", version = "~> 3.6" }
